7ba2d7bcc73e92ee7b9c3f2b1eced27b2eaaed07
KlaraKramer <46454343+KlaraKramer@users.noreply.github.com>
2019-04-19 11:11:27 +0200

    Update wecker.py

--- server/modules/wecker.py
+++ server/modules/wecker.py
@@ -73,19 +73,19 @@ def get_reply(tiane, dicanalyse):
     tag = time.get('day')
     stunde = time.get('hour')
     minute = time.get('minute')
-    tage = {'01': 'ersten', '02': 'zweiten', '03': 'dritten', '04': 'vierten', '05': 'fünften',
+    tage = {'01': 'ersten', '02': 'zweiten', '03': 'dritten', '04': 'vierten', '05': 'fÃ¼nften',
                 '06': 'sechsten', '07': 'siebten', '08': 'achten', '09': 'neunten', '10': 'zehnten',
-                '11': 'elften', '12': 'zwölften', '13': 'dreizehnten', '14': 'vierzehnten', '15': 'fünfzehnten',
+                '11': 'elften', '12': 'zwÃ¶lften', '13': 'dreizehnten', '14': 'vierzehnten', '15': 'fÃ¼nfzehnten',
                 '16': 'sechzehnten', '17': 'siebzehnten', '18': 'achtzehnten', '19': 'neunzehnten', '20': 'zwanzigsten',
                 '21': 'einundzwanzigsten', '22': 'zweiundzwanzigsten', '23': 'dreiundzwanzigsten', '24': 'vierundzwanzigsten',
-                '25': 'fünfundzwanzigsten', '26': 'sechsundzwanzigsten', '27': 'siebenundzwanzigsten', '28': 'achtundzwanzigsten',
-                '29': 'neunundzwanzigsten', '30': 'dreißigsten', '31': 'einunddreißigsten', '32': 'zweiunddreißigsten'}
-    Monate = {'01': 'Januar', '02': 'Februar', '03': 'März', '04': 'April', '05': 'Mai', '06': 'Juni',
+                '25': 'fÃ¼nfundzwanzigsten', '26': 'sechsundzwanzigsten', '27': 'siebenundzwanzigsten', '28': 'achtundzwanzigsten',
+                '29': 'neunundzwanzigsten', '30': 'dreiÃŸigsten', '31': 'einunddreiÃŸigsten', '32': 'zweiunddreiÃŸigsten'}
+    Monate = {'01': 'Januar', '02': 'Februar', '03': 'MÃ¤rz', '04': 'April', '05': 'Mai', '06': 'Juni',
                   '07': 'Juli', '08': 'August', '09': 'September', '10': 'Oktober', '11': 'November',
                   '12': 'Dezember'}
-    Stunden = {'01': 'ein', '02': 'zwei', '03': 'drei', '04': 'vier', '05': 'fünf', '06': 'sechs',
-               '07': 'sieben', '08': 'acht', '09': 'neun', '10': 'zehn', '11': 'elf', '12': 'zwölf',
-               '13': 'dreizehn', '14': 'vierzehn', '15': 'fünfzehn', '16': 'sechzehn', '17': 'siebzehn',
+    Stunden = {'01': 'ein', '02': 'zwei', '03': 'drei', '04': 'vier', '05': 'fÃ¼nf', '06': 'sechs',
+               '07': 'sieben', '08': 'acht', '09': 'neun', '10': 'zehn', '11': 'elf', '12': 'zwÃ¶lf',
+               '13': 'dreizehn', '14': 'vierzehn', '15': 'fÃ¼nfzehn', '16': 'sechzehn', '17': 'siebzehn',
                '18': 'achtzehn', '19': 'neunzehn', '20': 'zwanzig', '21': 'einundzwanzig', '22': 'zweiundzwanzig',
                '23': 'dreiundzwanzig', '24': 'vierundzwanzig'}
     if minute[0] == '0':
@@ -133,9 +133,20 @@ def handle(text, tiane, profile):
                     del liste[len(liste) - 3]
         
 
-def isValid(text):
-    text = text.lower()
-    if 'weck ' in text or 'wecke ' in text:
+def isValid(txt):
+    tt = txt.replace('.', (''))
+    tt = tt.replace('?', (''))
+    tt = tt.replace('!', (''))
+    tt = tt.replace('.', (''))
+    tt = tt.replace(',', (''))
+    tt = tt.replace('"', (''))
+    tt = tt.replace('(', (''))
+    tt = tt.replace(')', (''))
+    tt = tt.replace('â‚¬', ('Euro'))
+    tt = tt.replace('%', ('Prozent'))
+    tt = tt.replace('$', ('Dollar'))
+    text = tt.lower()
+    if 'weck ' or 'wecke ' in text:
         return True
 
 class Tiane:
