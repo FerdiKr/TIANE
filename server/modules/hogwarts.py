f081de594008d5f07850a3a57ffed2a23a54a466
KlaraKramer <46454343+KlaraKramer@users.noreply.github.com>
2019-04-19 11:06:50 +0200

    Update hogwarts.py

--- server/modules/hogwarts.py
+++ server/modules/hogwarts.py
@@ -1,18 +1,27 @@
 import random
 
-WORDS = ['Hogwarts', 'Gryffindor', 'Ravenclaw', 'Hufflepuff', 'Slytherin', 'Dumbledore']
-PRIORITY = 1
 
-def reply(text, tiane):
+def reply(txt, tiane):
     output = ''
-    text = text.lower()
+    tt = txt.replace('.', (''))
+    tt = tt.replace('?', (''))
+    tt = tt.replace('!', (''))
+    tt = tt.replace('.', (''))
+    tt = tt.replace(',', (''))
+    tt = tt.replace('"', (''))
+    tt = tt.replace('(', (''))
+    tt = tt.replace(')', (''))
+    tt = tt.replace('â‚¬', ('Euro'))
+    tt = tt.replace('%', ('Prozent'))
+    tt = tt.replace('$', ('Dollar'))
+    text = tt.lower()
     t = str.split(text)
     ind = 1
     a = False
     g_g = 'Mutig und tapfer schreiten wir hin, haben als Ziel den Sieg im Sinn, rot und gold auf unsrer Fahne, wir Gryffindors sind erste Sahne.'
-    r_g = 'Witzigkeit im Übermaß ist des Menschen größter Schatz.'
+    r_g = 'Witzigkeit im ÃœbermaÃŸ ist des Menschen grÃ¶ÃŸter Schatz.'
     h_g = 'In Hufflepuff ist man gerecht und treu, man hilft den anderen wo man kann und hat vor Arbeit keine Scheu.'
-    s_g = 'In Slytherin weiß man noch List und Tücke zu verbinden, dafür wirst du hier noch echte Freunde finden.'
+    s_g = 'In Slytherin weiÃŸ man noch List und TÃ¼cke zu verbinden, dafÃ¼r wirst du hier noch echte Freunde finden.'
     satz = {}
     rep = False
     if 'gryffindor' in text:
@@ -31,23 +40,22 @@ def reply(text, tiane):
         if 'in welchem hogwarts haus bist du' in text:
             output = 'Ich bin eine Ravenclaw. ' + r_g
         elif 'in welchem hogwarts haus bin ich' in text:
-            output = random.choice(['Wie würdest du dich denn einschätzen?', 'In welchem Haus wärst du denn gerne?', 'Wo wärst du denn gerne?', 'Wo ist denn dein Lieblingscharakter?'])
+            output = random.choice(['Wie wÃ¼rdest du dich denn einschÃ¤tzen?', 'In welchem Haus wÃ¤rst du denn gerne?', 'Wo wÃ¤rst du denn gerne?', 'Wo ist denn dein Lieblingscharakter?'])
             a = True
         elif 'kennst du hogwarts' in text:
-            output = 'Selbstverständlich kenne ich Hogwarts. ' + 'In welchem Hogwarts Haus bist du?'
+            output = 'SelbstverstÃ¤ndlich kenne ich Hogwarts. ' + 'In welchem Hogwarts Haus bist du?'
             a = True
         elif 'ich will nach hogwarts' in text:
-            output = 'Das kann ich nachvollziehen, ich wäre jetzt auch lieber auf einer Zaubererschule.'
+            output = 'Das kann ich nachvollziehen, ich wÃ¤re jetzt auch lieber auf einer Zaubererschule.'
             a = False
         else:
             output = random.choice(['Accio Feuerblitz', 'Alohomora', 'Nach all dieser Zeit?'])
             a = False
     elif 'dumbledore' in text:
-        d_1 = 'Es sind nicht unsere Fähigkeiten, die zeigen wer wir sind, sondern unsere Entscheidungen.'
+        d_1 = 'Es sind nicht unsere FÃ¤higkeiten, die zeigen wer wir sind, sondern unsere Entscheidungen.'
         d_2 = 'Obgleich wir von verschiedenen Orten kommen und verschiedene Sprachen sprechen, unsere Herzen schlagen gemeinsam.'
-        d_3 = 'Es tut nicht gut, nur seinen Träumen nachzuhängen und zu vergessen zu leben.'
-        #output = random.choice([d_1, d_2, d_3])
-        output = d_1
+        d_3 = 'Es tut nicht gut, nur seinen TrÃ¤umen nachzuhÃ¤ngen und zu vergessen zu leben.'
+        output = random.choice([d_1, d_2, d_3])
         a = False
     elif 'nach all dieser zeit' in text or 'nach all der zeit' in text:
         output = 'immer.'
@@ -60,9 +68,9 @@ def reply(text, tiane):
 
 def handle(text, tiane, profile):
     g_g = 'Mutig und tapfer schreiten wir hin, haben als Ziel den Sieg im Sinn, rot und gold auf unsrer Fahne, wir Gryffindors sind erste Sahne.'
-    r_g = 'Witzigkeit im Übermaß ist des Menschen größter Schatz.'
+    r_g = 'Witzigkeit im ÃœbermaÃŸ ist des Menschen grÃ¶ÃŸter Schatz.'
     h_g = 'In Hufflepuff ist man gerecht und treu, man hilft den anderen wo man kann und hat vor Arbeit keine Scheu.'
-    s_g = 'In Slytherin weis man noch List und Tücke zu verbinden, dafür wirst du hier noch echte Freunde finden.'
+    s_g = 'In Slytherin weis man noch List und TÃ¼cke zu verbinden, dafÃ¼r wirst du hier noch echte Freunde finden.'
     b = False
     dic = reply(text, tiane)
     ausgabe = dic.get('output')
@@ -74,7 +82,18 @@ def handle(text, tiane, profile):
             tiane.say('Das habe ich leider nicht verstanden')
             b = False
         else:
-            nt = neuertext.lower()
+            tt = neuertext.replace('.', (''))
+            tt = tt.replace('?', (''))
+            tt = tt.replace('!', (''))
+            tt = tt.replace('.', (''))
+            tt = tt.replace(',', (''))
+            tt = tt.replace('"', (''))
+            tt = tt.replace('(', (''))
+            tt = tt.replace(')', (''))
+            tt = tt.replace('â‚¬', ('Euro'))
+            tt = tt.replace('%', ('Prozent'))
+            tt = tt.replace('$', ('Dollar'))
+            nt = tt.lower()
             if 'gryffindor' in nt:
                 output_zwei = g_g
                 output_drei = 'Ich bin eine Ravenclaw.'
@@ -98,7 +117,18 @@ def handle(text, tiane, profile):
         if neuertext_zwei == 'TIMEOUT_OR_INVALID':
             tiane.say('Das habe ich leider nicht verstanden')
         else:
-            nt_z = neuertext_zwei.lower()
+            tt = neuertext_zwei.replace('.', (''))
+            tt = tt.replace('?', (''))
+            tt = tt.replace('!', (''))
+            tt = tt.replace('.', (''))
+            tt = tt.replace(',', (''))
+            tt = tt.replace('"', (''))
+            tt = tt.replace('(', (''))
+            tt = tt.replace(')', (''))
+            tt = tt.replace('â‚¬', ('Euro'))
+            tt = tt.replace('%', ('Prozent'))
+            tt = tt.replace('$', ('Dollar'))
+            nt_z = tt.lower()
             if 'gryffindor' in nt_z:
                 oz = g_g
                 od = 'Ich bin eine Ravenclaw.'
@@ -113,14 +143,25 @@ def handle(text, tiane, profile):
                 od = 'Ich bin eine Ravenclaw.'
             else:
                 oz = 'Es tut mir Leid.'
-                od = 'Ich habe dein Hogwarts Haus leider nicht verstehen können.'
+                od = 'Ich habe dein Hogwarts Haus leider nicht verstehen kÃ¶nnen.'
             tiane.say(oz)
             tiane.say(od)
+        
 
 
-
-def isValid(text):
-    text = text.lower()
+def isValid(txt):
+    tt = txt.replace('.', (''))
+    tt = tt.replace('?', (''))
+    tt = tt.replace('!', (''))
+    tt = tt.replace('.', (''))
+    tt = tt.replace(',', (''))
+    tt = tt.replace('"', (''))
+    tt = tt.replace('(', (''))
+    tt = tt.replace(')', (''))
+    tt = tt.replace('â‚¬', ('Euro'))
+    tt = tt.replace('%', ('Prozent'))
+    tt = tt.replace('$', ('Dollar'))
+    text = tt.lower()
     if 'hogwarts' in text or 'dumbledore' in text or 'ravenclaw' in text or 'gryffindor' in text or 'hufflepuff' in text or 'slytherin' in text or 'nach all der zeit' in text or 'nach all dieser zeit' in text:
         return True
 
@@ -140,7 +181,7 @@ def main():
     profile = {}
     tiane = Tiane()
     handle('Nach all der Zeit', tiane, profile)
-
-
+    
+    
 if __name__ == "__main__":
     main()
