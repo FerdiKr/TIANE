85ada6770a88bf226351b2a93736cb0fd92dcf8d
FerdiKr <46027993+FerdiKr@users.noreply.github.com>
2019-05-10 20:52:13 +0200

    Update reload_modules.py

--- server/modules/reload_modules.py
+++ server/modules/reload_modules.py
@@ -1,18 +1,31 @@
 import time
 
-def handle(text, tiane, profile):
+def reload_own(tiane):
     print('\n\n--------- RELOAD ---------')
-    tiane.say('Okay, warte einen Moment')
-    # Befehl an alle Räume senden
-    rooms_counter = 0
-    for room in tiane.rooms.values():
-        room.Clientconnection.send({'TIANE_reload_modules':True})
-        rooms_counter += 1
-
     # Eigene Module neu laden
     tiane.core.Modules.stop_continuous()
     tiane.core.Modules.load_modules()
     tiane.core.Modules.start_continuous()
+
+def handle(text, tiane, profile):
+    tiane.asynchronous_say('Okay, warte einen Moment')
+    rooms_counter = 0
+    reloaded = False
+
+    if 'server' in text.lower() or tiane.server_name in text.lower():
+        reload_own(tiane)
+        reloaded = True
+    elif not tiane.analysis['room'] == 'None':
+        # Befehl nur an einen bestimmten Raum senden
+        tiane.rooms[tiane.analysis['room']].Clientconnection.send({'TIANE_reload_modules':True})
+        rooms_counter += 1
+    else:
+        # Befehl an alle Räume senden
+        for room in tiane.rooms.values():
+            room.Clientconnection.send({'TIANE_reload_modules':True})
+            rooms_counter += 1
+        reload_own(tiane)
+        reloaded = True
 
     # Warten, bis die Räume fertig sind
     while rooms_counter > 0:
@@ -21,8 +34,9 @@ def handle(text, tiane, profile):
             if response is not None:
                 if response == True:
                     rooms_counter -= 1
-    time.sleep(1)
-    print('--------- FERTIG ---------\n\n')
+    if reloaded:
+        time.sleep(1)
+        print('--------- FERTIG ---------\n\n')
     tiane.say('Die Module wurden neu geladen.')
 
 
