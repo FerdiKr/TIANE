91e118f287db156ebc443f0e9665b18cae125e53
FerdiKr <46027993+FerdiKr@users.noreply.github.com>
2019-05-11 00:05:26 +0200

    Add some useless modules

---
+++ server/modules/thank_you.py
@@ -0,0 +1,9 @@
+PRIORITY = -1
+
+def isValid(text):
+    text = text.lower()
+    if 'danke' in text or 'thx' in text or 'thanks' in text:
+        return True
+
+def handle(text, tiane, local_storage):
+    tiane.say('[Gerne doch|Keine Ursache], {}.'.format(tiane.user))
