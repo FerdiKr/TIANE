55aad5a67e3621840716256dbbb35ec0065a1d5f
FerdiKr <46027993+FerdiKr@users.noreply.github.com>
2019-05-12 17:47:10 +0200

    Delete Hello_world.py

--- server/modules/Hello_world.py
+++
@@ -1,7 +0,0 @@
-def isValid(text):
-    text = text.lower()
-    if 'hallo' in text or text == 'hi' or text == '/start':
-        return True
-
-def handle(text, tiane, local_storage):
-    tiane.say('Hallo ' + tiane.user)
