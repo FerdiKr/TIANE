23d2f8b0a621934e3c7609551055e0d55d63788a
KlaraKramer <46454343+KlaraKramer@users.noreply.github.com>
2019-05-13 15:15:23 +0200

    Update erinnern.py

--- server/modules/erinnern.py
+++ server/modules/erinnern.py
@@ -150,11 +150,21 @@ def get_text(tiane, txt):
                     except TypeError:
                         ausgabe = ausgabe
     ausgabe = ausgabe.replace('übermorgen ', (' '))
-
     ausgabe = ausgabe.replace('morgen ', (' '))
-
     ausgabe = ausgabe.replace('daran ', (' '))
-
+    ausgabe = ausgabe.replace('ich', ('du'))
+    ausgabe = ausgabe.replace('mich', ('dich'))
+    if 'dass ' in text:
+        lang = len(ausgabe)
+        if ausgabe[(lang-1):] == ' ':
+            ausgabe = ausgabe[:(lang-1)]
+        l = len(ausgabe)
+        if ausgabe[(l-2):] == 'st':
+            ausgabe = ausgabe
+        elif ausgabe[(l-1):] == 's':
+            ausgabe = ausgabe + 't'
+        else:
+            ausgabe = ausgabe + 'st'
     return ausgabe
 
 
@@ -162,129 +172,69 @@ def get_text(tiane, txt):
 
 
 def get_time_for_reply(dicanalyse):
-
     now = datetime.datetime.now()
-
     time = dicanalyse.get('time')
-
     jahr = time.get('year')
-
     if jahr == 'None':
-
         jahr = str(now.year)
-
     tag = time.get('day')
-
     if tag == 'None':
-
         tag = str(now.day)
-
         if int(tag) <= 9:
-
             tag = '0' + tag
-
     monat = time.get('month')
-
     if monat == 'None':
-
         monat = str(now.month)
-
         if int(monat) <= 9:
-
             monat = '0' + str(monat)
-
     stunde = time.get('hour')
-
     if stunde == 'None':
-
          stunde = str(now.hour) #08
-
          if int(stunde) <= 9:
-
              stunde = '0' + stunde
-
     minute = time.get('minute')
-
     if minute == 'None':
-
          minute = '00'
-
     zeit = {'year': jahr, 'month': monat, 'day': tag, 'hour': stunde, 'minute': minute}
-
     return zeit
 
 
 
-def get_reply(tiane, dicanalyse):
-
+def get_reply_time(tiane, dicanalyse):
     time = get_time_for_reply(dicanalyse)
-
     jahr = time.get('year')
-
     monat = time.get('month')
-
     tag = time.get('day')
-
     stunde = time.get('hour')
-
     minute = time.get('minute')
-
     tage = {'01': 'ersten', '02': 'zweiten', '03': 'dritten', '04': 'vierten', '05': 'fünften',
-
                 '06': 'sechsten', '07': 'siebten', '08': 'achten', '09': 'neunten', '10': 'zehnten',
-
                 '11': 'elften', '12': 'zwölften', '13': 'dreizehnten', '14': 'vierzehnten', '15': 'fünfzehnten',
-
                 '16': 'sechzehnten', '17': 'siebzehnten', '18': 'achtzehnten', '19': 'neunzehnten', '20': 'zwanzigsten',
-
                 '21': 'einundzwanzigsten', '22': 'zweiundzwanzigsten', '23': 'dreiundzwanzigsten', '24': 'vierundzwanzigsten',
-
                 '25': 'fünfundzwanzigsten', '26': 'sechsundzwanzigsten', '27': 'siebenundzwanzigsten', '28': 'achtundzwanzigsten',
-
                 '29': 'neunundzwanzigsten', '30': 'dreißigsten', '31': 'einunddreißigsten', '32': 'zweiunddreißigsten'}
-
     Monate = {'01': 'Januar', '02': 'Februar', '03': 'März', '04': 'April', '05': 'Mai', '06': 'Juni',
-
                   '07': 'Juli', '08': 'August', '09': 'September', '10': 'Oktober', '11': 'November',
-
                   '12': 'Dezember'}
-
     Stunden = {'01': 'ein', '02': 'zwei', '03': 'drei', '04': 'vier', '05': 'fünf', '06': 'sechs',
-
                '07': 'sieben', '08': 'acht', '09': 'neun', '10': 'zehn', '11': 'elf', '12': 'zwölf',
-
                '13': 'dreizehn', '14': 'vierzehn', '15': 'fünfzehn', '16': 'sechzehn', '17': 'siebzehn',
-
                '18': 'achtzehn', '19': 'neunzehn', '20': 'zwanzig', '21': 'einundzwanzig', '22': 'zweiundzwanzig',
-
                '23': 'dreiundzwanzig', '24': 'vierundzwanzig'}
-
     if minute[0] == '0':
-
         mine = minute[1]
-
         if mine == '0':
-
             mine = ''
-
         else:
-
             mine = mine
-
     else:
-
         mine = minute
-
     day = tage.get(tag)
-
     month = Monate.get(str(monat))
-
     hour = Stunden.get(str(stunde))
-
     zeit_der_erinnerung = str(day) + ' ' + str(month) + ' um ' + str(hour) + ' Uhr ' + str(mine)
-
-    reply = 'Alles klar, ich sage dir am ' + zeit_der_erinnerung + ' bescheid, ' 
-
+    reply = zeit_der_erinnerung
     return reply
 
    
@@ -299,11 +249,13 @@ def handle(text, tiane, profile):
             tiane.local_storage['Erinnerungen'].append(E_eins)
         else:
             tiane.local_storage['Erinnerungen'] = [E_eins]
-        rep = get_reply(tiane, tiane.analysis)
+        rep = get_reply_time(tiane, tiane.analysis)
         if 'dass ' in r:
-            antwort = rep + r + '.'
+            antwort = 'Alles klar, ich sage dir am ' + rep + ' bescheid, ' + r + '.' ###
+        elif 'ans ' in text:
+            antwort = 'Alles klar, ich erinnere dich am ' + rep + ' ans ' + r + '.'
         else:
-            antwort = ' dass du ' + rep + r + ' musst.'
+            antwort = 'Alles klar, ich sage dir am ' + rep + ' bescheid, dass du ' + r + ' musst.'
         tiane.say(antwort)
     else:
         liste = tiane.local_storage.get('Erinnerungen')
@@ -336,6 +288,7 @@ def isValid(txt):
     tt = tt.replace('%', ('Prozent'))
     tt = tt.replace('$', ('Dollar'))
     text = tt.lower()
+    print(tt)
     if 'erinner' in text or 'erinnere' in text:
         return True
     else:
@@ -347,7 +300,7 @@ class Tiane:
     def __init__(self):
         self.local_storage = {}
         self.user = 'Baum'
-        self.analysis = {'room': 'None', 'time': {'month': 'None', 'hour': 'None', 'year': 'None', 'minute': 'None', 'day': 'None'}, 'town': 'None'}
+        self.analysis = {'room': 'None', 'time': {'month': '03', 'hour': '14', 'year': '2019', 'minute': '46', 'day': '13'}, 'town': 'None'}
 
 
     def say(self, text):
@@ -357,4 +310,8 @@ class Tiane:
 def main():
     profile = {}
     tiane = Tiane()
-    handle('Erinner mich in 20 Minuten dass die Präsentation zu Ende ist', tiane, profile)
+    handle('Erinner mich in 3 Minuten ans Kuchen backen', tiane, profile)
+
+
+if __name__ == "__main__":
+    main()
