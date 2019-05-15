39988ffaa3380b583021dc12ea430bb52e86b123
KlaraKramer <46454343+KlaraKramer@users.noreply.github.com>
2019-05-15 14:10:44 +0200

    Update smalltalk.py

--- server/modules/smalltalk.py
+++ server/modules/smalltalk.py
@@ -6,7 +6,7 @@ def isValid(text):
         return True
     if ('wer' in text or 'was' in text) and 'bist' in text and 'du' in text:
         return True
-    elif ('woher' in text or 'bedeutet' in text or 'heißt' in text) and ' name' in text and ('dein' in text or 'der' in text or 'tiane' in text):
+    if ('woher' in text or 'bedeutet' in text or 'heißt' in text) and ' name' in text :
         return True
     if (('was' in text and 'für' in text) or ('wie' in text and 'heißt' in text) or 'welches' in text) and 'buch' in text:
         return True
@@ -20,6 +20,10 @@ def isValid(text):
         return True
     if 'bist' in text and 'dumm' in text:
         return True
+    if 'bist' in text and 'toll' in text or 'bist' in text and 'genial' in text:
+        return True
+    if 'liebe' in text and 'dich' in text:
+        return True
 
 def handle(text, tiane, profile):
     text = text.lower()
@@ -27,7 +31,7 @@ def handle(text, tiane, profile):
         tiane.say('Ich heiße TIANE.')
     elif ('wer' in text or 'was' in text) and 'bist' in text and 'du' in text:
         tiane.say('Ich bin TIANE, ein Open-Source-Sprachassistent!')
-    elif ('woher' in text or 'bedeutet' in text or 'heißt' in text) and ' name' in text and ('dein' in text or 'der' in text or 'tiane' in text):
+    elif ('woher' in text or 'bedeutet' in text or 'heißt' in text) and ' name' in text :
         tiane.say('Anders als man denken könnte, ist mein Name tatsächlich keine Abkürzung für irgendwas. '
                   'Aber er ist angelehnt an einen sprechenden Computer namens "DIANE" aus einem von Ferdis Lieblingsbüchern.')
     elif (('was' in text and 'für' in text) or ('wie' in text and 'heißt' in text) or 'welches' in text) and 'buch' in text:
@@ -36,7 +40,7 @@ def handle(text, tiane, profile):
         tiane.say('Sagen wir mal so, den Turing-Test bestehe ich leider noch nicht... '
                   'Aber ich kann dir zum Beispiel das Wetter ansagen, ein paar allgemeine Wissensfragen beantworten '
                   'rechnen, würfeln und so weiter. '
-                  'Und für alles weitere bist du gerne eingeladen, selbst in meinem GitHub-Repository aktiv zu werden!')
+                  'Und für alles weitere bist du gerne eingeladen, selbst auf in meinem GitHub-Repository aktiv zu werden!')
     elif '😂' in text:
         tiane.say('Warum lachst du? 😂')
         response = tiane.listen()
@@ -51,3 +55,7 @@ def handle(text, tiane, profile):
         tiane.say('Frag mal was vernünftiges ;)')
     elif 'bist' in text and 'dumm' in text:
         tiane.say('Nein, du stellst nur die falschen Fragen.')
+    elif 'bist' in text and 'toll' in text or 'bist' in text and 'genial' in text:
+        tiane.say(random.choice(['Vielen Dank, {}'.format(tiane.user), 'Dankeschön, {}'.format(tiane.user), 'Es freut mich, dass ich hilfreich bin!'.format(tiane.user)]))
+    elif 'liebe' in text and 'dich' in text:
+        tiane.say(random.choice(['Eine wirklich schlechte Entscheidung... aber Danke!', 'Ich fühle mich geehrt, {}'.format(tiane.user), 'Ich fürchte, als Sprachassistent bin ich eine schlechte Wahl für deinen Lebensgefährten...']))
