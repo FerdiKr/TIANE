75c58930ed4e82b550c06e0f7b5049fe54b93bd2
JosuaKugler <50653379+JosuaKugler@users.noreply.github.com>
2019-05-18 11:52:58 +0200

    Update hanoi.py
    added movetime method

--- server/modules/hanoi.py
+++ server/modules/hanoi.py
@@ -190,6 +190,113 @@ def movenumber(text):
         ret = "Ich konnte leider keine Zugzahl ermitteln
     return ret, not nosuccess
 
+def possibilitynumber(n,k):
+    text = text.lower()
+    textlist = re.split("\s", text)
+    success = True
+    for index, word in enumerate(textlist):
+        if index > 1:
+            if re.search(r"feld|felder|platz|plätze|stab|stäbe", word):
+                try:
+                    k = int(textlist[index-1]
+                except:
+                    success = False
+            if re.search(r"scheibe|scheiben|klotz|klötze", word):
+                try:
+                    n = int(textlist[index-1])
+                except:
+                    success = False
+    if success:
+        ret = "Um die Türme von Hanoi mit {} Scheiben und {} Felder optimal zu lösen, gibt es {} Möglichkeiten.".format(n, k , upsilon(n,k))
+    else:
+        ret = "Ich konnte die Anzahl der Möglichkeiten leider nicht ermitteln
+    return ret, success
+
+def movetime(text):
+    text = text.lower()
+    textlist = re.split("\s", text)
+    nosuccess = False
+    for index, word in enumerate(textlist):
+        if index > 1:
+            if re.search(r"feld|felder|platz|plätze|stab|stäbe", word):
+                try:
+                    k = int(textlist[index-1]
+                except:
+                    nosuccess = True
+            if re.search(r"scheibe|scheiben|klotz|klötze", word):
+                try:
+                    n = int(textlist[index-1])
+                except:
+                    nosuccess = True
+    if not nosuccess:
+        m = M(n,k)
+        years = int(m/(3600*24*365))
+        remaining = m-years *3600*24*365
+        months = int(remaining/(3600*24*(365/12)))
+        remaining -= months * 3600*24*(365/12)
+        days = int(remaining/(3600*24))
+        remaining -= days *3600*24
+        hours = int(remaining/(3600))
+        remaining -= hours * 3600
+        minutes = int(remaining/60)
+        remaining -= minutes * 60
+        seconds = remaining
+
+        time_string = ""
+        if years > 0:
+            if years ==1:
+                time_string += "1 Jahr"
+            else:
+                time_string += str(years) + " Jahre"
+        
+        if months > 0:
+            time_string += ","
+            if months == 1:
+                time_string += "1 Monat"
+            else:
+                time_string += str(months) + " Monate"
+        
+        if days > 0:
+            time_string += ","
+            if months == 1:
+                time_string += "1 Tag"
+            else:
+                time_string += str(days) + "Tage"
+        
+        if days > 0:
+            time_string += ","
+            if days == 1:
+                time_string += "1 Tag"
+            else:
+                time_string += str(days) + "Tage"
+
+        if hours > 0:
+            time_string += ","
+            if hours == 1:
+                time_string += "1 Stunde"
+            else:
+                time_string += str(hours) + "Stunden"
+        
+        if minutes > 0:
+            time_string += ","
+            if minutes == 1:
+                time_string += "1 Minute"
+            else:
+                time_string += str(minutes) + "Minuten"
+        
+        if seconds > 0:
+            time_string += ","
+            if seconds == 1:
+                time_string += "1 Sekunde"
+            else:
+                time_string += str(seconds) + "Sekunden"
+
+        ret = "Wenn man pro Sekunde einen Zug macht, benötigt man für {} Scheiben und {} Felder {}.".format(n, k , timestring)
+    else:
+        ret = "Ich konnte leider keine Zugzahl ermitteln
+    sucess = not nosuccess
+    return ret, sucess
+                            
 def handle(txt, tiane, profile):
     '''
     tt = txt.replace('?', (''))
@@ -243,8 +350,7 @@ def handle(txt, tiane, profile):
     if weiter:
         for key in keyPossibilities:
             if key in text:
-                # answer, success = movenumber(text)
-                # Methode für Possibilities
+                answer, success = possibilitynumber(text)
                 weiter = False
                 break
 
