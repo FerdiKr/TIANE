9d002ab80f8af2b05631a48db528a88cde3e84f2
Emile <hanemile@protonmail.com>
2019-05-23 20:42:04 +0200

    simple qr module

---
+++ server/modules/qr.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+import urllib.request
+
+def isValid(text):
+    text = text.lower()
+    if 'qr' in text:
+        return True
+    else:
+        return False
+
+def handle(text, tiane, profile):
+
+    tiane.say('Was möchtest du in den qr-code schreiben?')
+
+    # Frage den Nutzer nach Inhalt
+    antwort = tiane.listen()
+    if antwort == 'TIMEOUT_OR_INVALID':
+        tiane.say('Ich konnte dich leider nicht verstehen')
+    else:
+        url = f"https://api.qrserver.com/v1/create-qr-code/?size=150x150&data={antwort}"
+        tiane.say(url)
