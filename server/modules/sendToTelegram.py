5a1419c60e3ee0a2b18ef40c970a27b547a1f347
petterpet <peter2710e@gmail.com>
2019-06-01 15:15:36 +0200

    changed return-behaviour, added match-regex

--- server/modules/sendToTelegram.py
+++ server/modules/sendToTelegram.py
@@ -2,8 +2,8 @@ import re
 
 # Beschreibung
 '''
-Mit diesem Modul kann sich ein User Nachrichten per Telegram zuschicken lassen.
-Dazu sagt er beispielsweise "Sende <text> an mein Smartphone".
+Mit diesem Modul kann man sich Nachrichten per Telegram zuschicken lassen.
+Dazu sagt man "Sende <text> an mein Smartphone" oder "Smartphone Nachricht <text>".
 '''
 
 def isValid(text):
@@ -14,12 +14,31 @@ def isValid(text):
         return False
 
 def handle(text, tiane, local_storage):
-    if tiane.telegram_call:
-        tiane.say('Du hast geschrieben: ' + text)
+    text = text.lower()
+    length = len(text)
+
+    match = re.search('^smartphone nachricht', text)
+    if match != None:
+        end = match.end() + 1
+        nachricht = text[end:length]
+
     else:
-        # Nachricht per Telegram senden
-        # Nachricht filtern
+        liste = re.split('\s', text)
+        elements = len(liste)
+        if liste[0] == 'sende' and liste[elements-1] == 'smartphone':
+            nachricht = ''
+            for i in range(1,elements):
+                if liste[i] == 'an' and liste[i+1] == 'mein':
+                    break
+                else:
+                    nachricht += liste[i]
+                    nachricht += ' '
 
-        nachricht = ''
-
-        tiane.say(nachricht, output = 'telegram')
+    if nachricht != '':
+        if tiane.telegram_call:
+            tiane.say('Du hast folgende Nachricht an dich selbst geschrieben:')
+        else:
+            tiane.say("Nachricht an dich:", output='telegram')
+        tiane.say(nachricht, output='telegram')
+    else:
+        tiane.say("Ich konnte deine Nachricht nicht heraus filtern")
