ea13b4ac2ec157290fde8d7dd6dbd3b308349b22
KlaraKramer <46454343+KlaraKramer@users.noreply.github.com>
2019-07-26 20:24:13 +0200

    Update countdown.py

--- server/modules/countdown.py
+++ server/modules/countdown.py
@@ -5,52 +5,13 @@ import random
 def get_date(text, tiane):
     tagesdifferenz = ''
     now = datetime.datetime.now()
-    datum = ''
-    jetzt_jahr = now.year
-    jetzt_monat = now.month
-    jetzt_tag = now.day
-    time = tiane.analysis.get('time')
-    jahr = time.get('year')
-    if jahr == 'None':
-        jahr = str(now.year)
-    tag = time.get('day')
-    if tag == 'None':
-        tag = str(now.day)
-    monat = time.get('month')
-    if monat == 'None':
-        if int(tag) == 28:
-            if now.month == 2:
-                if int(jahr) / 4 == 0:
-                    monat = '3'
-                else:
-                    monat = '2'
-            else:
-                monat = str(now.month)
-        elif int(tag) == 29:
-            if now.month == 2:
-                monat = '3'
-            else:
-                monat = str(now.month)
-        elif int(tag) == 30:
-            if now.month == 4 or now.month == 6 or now.month == 9 or now.month == 11:
-                monat = str(now.month + 1)
-            else:
-                monat = str(now.month)
-        elif int(tag) == 31:
-            monat = str(now.month + 1)
-        else:
-            monat = str(now.month)
-    monate = {1: 'Jan', 2: 'Feb', 3: 'Mar', 4: 'Apr', 5: 'May', 6: 'Jun', 7: 'Jul', 8: 'Aug', 9: 'Sep', 10: 'Oct', 11: 'Nov', 12: 'Dec'}
-    monat = int(monat)
-    month_in_format = str(monate.get(monat))
-    xs = ''
-    xs = xs + str(tag) + ' '
-    xs = xs + month_in_format + ' '
-    xs = xs + str(jahr)
-    ts = datetime.datetime.now()
-    heute = ts.strftime('%d %b %Y')
-    diff =  datetime.datetime.strptime(xs, '%d %b %Y') - datetime.datetime.strptime(heute, '%d %b %Y')
-    daynr = diff.days
+    found_time = tiane.analysis.get('datetime')
+    if found_time < now:
+        event_time = str(found_time)
+        found_time = event_time.replace(str(now.year), str((now.year + 1)))
+        found_time = datetime.datetime.strptime(found_time, '%Y-%m-%d %H:%M:%S.%f')
+    time_to_event = abs(found_time - now)
+    daynr = time_to_event.days
     tagesdifferenz = 'in ' + str(daynr) + ' Tagen'
     if tagesdifferenz == '':
         if jetzt_jahr == jahr and jetzt_monat == monat and jetzt_tag == tag:
@@ -73,55 +34,6 @@ def get_date(text, tiane):
                 tagesdifferenz = 'in ' + str(tagesanzahl) + ' Tagen'
     return tagesdifferenz
 
-
-def get_past_date(text, tiane):
-    now = datetime.datetime.now()
-    jetzt_jahr = now.year
-    jetzt_monat = now.month
-    jetzt_tag = now.day
-    time = tiane.analysis.get('time')
-    jahr = time.get('year')
-    if jahr == 'None':
-        jahr = str(now.year)
-    tag = time.get('day')
-    if tag == 'None':
-        tag = str(now.day)
-    monat = time.get('month')
-    if monat == 'None':
-        if int(tag) == 28:
-            if now.month == 2:
-                if int(jahr) / 4 == 0:
-                    monat = '3'
-                else:
-                    monat = '2'
-            else:
-                monat = str(now.month)
-        elif int(tag) == 29:
-            if now.month == 2:
-                monat = '3'
-            else:
-                monat = str(now.month)
-        elif int(tag) == 30:
-            if now.month == 4 or now.month == 6 or now.month == 9 or now.month == 11:
-                monat = str(now.month + 1)
-            else:
-                monat = str(now.month)
-        elif int(tag) == 31:
-            monat = str(now.month + 1)
-        else:
-            monat = str(now.month)
-
-    d0 = date(int(jetzt_jahr), int(jetzt_monat), int(jetzt_tag))
-    d1 = date(int(jahr), int(monat), int(tag))
-    delta = d0 - d1
-    delta = str(delta)
-    elemente = str.split(delta)
-    tagesanzahl = elemente[0]
-    if tagesanzahl == 1:
-        differenz = 'gestern'
-    else:
-        differenz = 'vor ' + str(tagesanzahl) + ' Tagen'
-    return differenz
 
 def get_birthday(txt, tiane):
     tt = txt.replace('.', (''))
@@ -176,11 +88,7 @@ def handle(txt, tiane, profile):
         ausgabe = get_birthday(text, tiane)
     else:
         datum = get_date(text, tiane)
-        if datum == '':
-            datum = get_past_date(text, tiane)
-            ausgabe = random.choice([datum + ' war das Ereignis.', 'Das Ereignis war ' + datum + '.'])
-        else:
-            ausgabe = random.choice([datum + ' ist das Ereignis.', 'Das Ereignis ist ' + datum + '.'])
+        ausgabe = random.choice([datum + ' ist das Ereignis.', 'Das Ereignis ist ' + datum + '.'])
     tiane.say(ausgabe)
 
 def isValid(txt):
@@ -197,7 +105,7 @@ def isValid(txt):
     tt = tt.replace('$', ('Dollar'))
     text = tt.lower()
     if 'wie' in text:
-        if 'lange' in text or 'tage' in text and 'viele' in text or 'wann ' in text and 'geburtstag' in text:
+        if 'lange' in text or 'tage' in text and 'viele' in text:
             return True
     if 'geburtstag' in text:
         return True
@@ -206,7 +114,7 @@ class Tiane:
     def __init__(self):
         self.local_storage = {}
         self.user = 'Baum'
-        self.analysis = {'room': 'None', 'time': {'month': '09', 'hour': '05', 'year': '2018', 'minute': '00', 'day': '19'}, 'town': 'None'}
+        self.analysis = {'town': None, 'room': None, 'rooms': [None, 'Schlafzimmer'], 'datetime': datetime.datetime(2019, 6, 4, 12, 43, 35, 718049), 'time': {'day': 4, 'month': 6, 'year': 2019, 'hour': 12, 'minute': 43, 'second': 35}}
 
     def say(self, text):
         print (text)
@@ -217,6 +125,6 @@ class Tiane:
 def main():
     profile = {}
     tiane = Tiane()
-    handle('Vor wie vielen Tagen war Ferdis Geburtstag', tiane, profile)
+    handle('In wie vielen Tagen ist der 4. Mai?', tiane, profile)
 if __name__ == '__main__':
     main()
