f0fdf0d21cf8d0ad56aacce55b1ddbe1c1ee3f59
FerdiKr <46027993+FerdiKr@users.noreply.github.com>
2019-07-30 21:09:54 +0200

    Add module for future feature "reload_users"

---
+++ server/modules/reload_users.py
@@ -0,0 +1,16 @@
+import time
+
+def isValid(text):
+    text = text.lower()
+    if 'lad' in text and 'nutzer' in text:
+        return True
+    else:
+        return False
+
+def handle(text, tiane, profile):
+    tiane.asynchronous_say('Okay, warte einen Moment')
+    # Einfach der Nutzer-Klasse den Laden-Befehl geben...
+    tiane.Users.load_users()
+    time.sleep(1)
+    print('--------- FERTIG ---------\n\n')
+    tiane.say('Die Nutzer wurden neu geladen.')
